af1955ee9097b30bb2e6b38aacb8c17009b7f749
DIPNARAYAN ROY <dipnarayan.roy@gmail.com>
2020-08-27 02:53:20 +0530

    log in page

---
+++ travelRecord/travelRecord/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace travelRecord
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void loginButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+}
